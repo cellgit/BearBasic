4aa7b6dc8f25b10a2479e5f13da42e36db30caaa
liuhongli <>
2024-12-10 08:47:02 +0800

    [t0]xx

--- Sources/BearBasic/BearBasic.swift
+++ Sources/BearBasic/BearBasic.swift
@@ -9,38 +9,22 @@ public class BearBasic {
     
     @MainActor public static let shared = BearBasic.init()
     
+    
+    /// 启动sdk,需要提前设置的参数,可以在这里设置
+    /// - Parameter appId: appId
     public func start(with appId: String) {
         /// 存储appId
         UserDefaultsManager.save(appId, forKey: UserDefaultsKey.appId.key)
         
-        ///  生成uuid
-        ///  获取idfv
-        ///`获取idfa`
-        /// 获取platform
-        /// appVersion
-        /// bundleId
-        /// attStatus
-        
-        
         // 生成 UUID
         let uuid = UUID().uuidString
         UserDefaultsManager.save(uuid, forKey: UserDefaultsKey.uuid.key)
-        
-
-        
-        
 
     }
-    
-    
-//    public func setAppId(with appId: String) {
-//        UserDefaultsManager.save(appId, forKey: UserDefaultsKey.appId.key)
-//    }
     
     public func getAppId() -> String? {
         UserDefaultsManager.get(forKey: UserDefaultsKey.appId.key, ofType: String.self)
     }
-    
 
     
 }
@@ -48,48 +32,12 @@ public class BearBasic {
 
 extension BearBasic {
     
-//    private func getIDFA(completion: @escaping (String) -> Void) {
-//        if #available(iOS 14, *) {
-//            ATTrackingManager.requestTrackingAuthorization { status in
-//                if status == .authorized {
-//                    completion(ASIdentifierManager.shared().advertisingIdentifier.uuidString)
-//                } else {
-//                    completion("unavailable")
-//                }
-//            }
-//        } else {
-//            let idfa = ASIdentifierManager.shared().isAdvertisingTrackingEnabled ? ASIdentifierManager.shared().advertisingIdentifier.uuidString : "unavailable"
-//            completion(idfa)
-//        }
-//    }
-//    
-//    @available(iOS 14, *)
-//    private func getATTStatusString(_ status: ATTrackingManager.AuthorizationStatus) -> String {
-//        switch status {
-//        case .notDetermined: return "notDetermined"
-//        case .restricted: return "restricted"
-//        case .denied: return "denied"
-//        case .authorized: return "authorized"
-//        @unknown default: return "unknown"
-//        }
-//    }
-    
     private var isIOS: Bool {
 #if os(iOS)
         return true
 #else
         return false
 #endif
-    }
-    
-    
-    @MainActor private func getIDFV() -> String {
-        if isIOS {
-            return UIDevice.current.identifierForVendor?.uuidString ?? "unknown"
-        } else {
-            // 在 macOS 上使用自定义逻辑
-            return ProcessInfo.processInfo.globallyUniqueString
-        }
     }
     
 }
